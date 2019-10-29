cbc9771e4fccf4623f85da1c58914a1dddd947f1
Grey <sergejyurskyj@yahoo.com>
2019-10-28 21:03:00 -0700

    Commit 70. SQL DELETE data from a table

---
+++ Section 7 Introduction to PostgreSQL/70. SQL DELETE data from a table.py
@@ -0,0 +1,6 @@
+# -- DELETE FROM items; #be carefull delete everything
+# -- DELETE FROM items WHERE id=4
+# SELECT *FROM purchases;
+# DELETE FROM purchases WHERE item_id=4;
+# SELECT *FROM items;
+# DELETE FROM items WHERE id=4;
