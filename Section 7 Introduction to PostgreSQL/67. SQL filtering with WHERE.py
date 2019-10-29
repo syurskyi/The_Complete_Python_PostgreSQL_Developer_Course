bf74aaa976bee0f3ff90a1453ff4e40115284eb2
Grey <sergejyurskyj@yahoo.com>
2019-10-28 20:49:24 -0700

    Commit 67. SQL filtering with WHERE

---
+++ Section 7 Introduction to PostgreSQL/67. SQL filtering with WHERE.py
@@ -0,0 +1,4 @@
+# -- SELECT customers.first_name, customers.last_name FROM customers
+# # -- WHERE customers.first_name = 'Rolf';
+# # SELECT customers.first_name, customers.last_name FROM customers
+# # WHERE customers.first_name = 'Rolf' OR customers.last_name = 'Watson';
