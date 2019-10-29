f6858ed157719e3b72d3aae64d0a25dcb9e63f3a
Grey <sergejyurskyj@yahoo.com>
2019-10-28 20:58:06 -0700

    Commit 69. SQL UPDATE data in a table

---
+++ Section 7 Introduction to PostgreSQL/69. SQL UPDATE data in a table.py
@@ -0,0 +1,4 @@
+# SELECT * FROM items;
+# -- UPDATE items SET price=4.00 WHERE id=3;
+# -- UPDATE items SET price=5.00 WHERE price=4;
+# UPDATE items SET price=10.00 # be carefull this operation change prices for all itmes
