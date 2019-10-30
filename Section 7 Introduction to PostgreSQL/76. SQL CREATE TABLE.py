04dbd4da3c868613f0c4f3368679309dc51cf83e
syurskyi <sergejyurskyj@yahoo.com>
2019-10-29 19:53:31 -0700

    Create 76. SQL CREATE TABLE.py

---
+++ Section 7 Introduction to PostgreSQL/76. SQL CREATE TABLE.py
@@ -0,0 +1,18 @@
+CREATE TABLE public.users(
+id INTEGER PRIMARY KEY,
+name CHARACTER VARYING(100) NOT NULL,
+)
+
+CREATE TABLE public.users(
+id INTEGER,
+name CHARACTER VARYING(100) NOT NULL,
+CONSTAINT users_id_pkey PRIMARY KEY (id)
+)
+
+SELECT * FROM public.users;
+
+CREATE TABLE public.videos(
+id INTEGER PRIMARY KEY,
+user_id INTEGER REFERENCES public.users,
+name CHARACTER VARYING(255) NOT NULL
+)
