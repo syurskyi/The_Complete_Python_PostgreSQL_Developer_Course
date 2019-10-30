2622625a6fc2f76522b5f5ec756f7b87cf2b3875
Grey <sergejyurskyj@yahoo.com>
2019-10-29 22:12:40 -0700

    Commit Section 7 Update

--- Section 7 Introduction to PostgreSQL/74. SQL GROUP BY for aggregation of data.py
+++ Section 7 Introduction to PostgreSQL/74. SQL GROUP BY for aggregation of data.py
@@ -1,13 +1,13 @@
-SELECT * FROM customers LEFT JOIN purchases ON customers.id = purchases.customers_id;
-
-SELECT customers.first_name, customers.last_name, purchases.id FROM customers LEFT JOIN purchases ON customers.id = purchases.customers_id;
-
-SELECT customers.first_name, customers.last_name, COUNT(purchases.id) FROM customers LEFT JOIN purchases ON customers.id = purchases.customers_id
-GROUP BY customers.id;
-
-SELECT *FROM customers GROUP BY customers.id;
-
-SELECT customers.first_name, customers.last_name, SUM(items.price) FROM items 
-INNER JOIN purchases ON items.id = purchases.item_id
-INNER JOIN customers ON purchases.customers_id = customers.id
-GROUP BY customers.id ;
+# SELECT * FROM customers LEFT JOIN purchases ON customers.id = purchases.customers_id;
+#
+# SELECT customers.first_name, customers.last_name, purchases.id FROM customers LEFT JOIN purchases ON customers.id = purchases.customers_id;
+#
+# SELECT customers.first_name, customers.last_name, COUNT(purchases.id) FROM customers LEFT JOIN purchases ON customers.id = purchases.customers_id
+# GROUP BY customers.id;
+#
+# SELECT *FROM customers GROUP BY customers.id;
+#
+# SELECT customers.first_name, customers.last_name, SUM(items.price) FROM items
+# INNER JOIN purchases ON items.id = purchases.item_id
+# INNER JOIN customers ON purchases.customers_id = customers.id
+# GROUP BY customers.id ;
--- Section 7 Introduction to PostgreSQL/75. SQL ORDER BY for sorting.py
+++ Section 7 Introduction to PostgreSQL/75. SQL ORDER BY for sorting.py
@@ -1,35 +1,35 @@
-SELECT * FROM purchases INNER JOIN items on purchases.item_id = items.id;
-
-SELECT SUM(items.price) FROM purchases INNER JOIN items on purchases.item_id = items.id;
-
-SELECT * FROM items
-INNER JOIN purchases ON purchases.item_id = items.id
-INNER JOIN customers OM purchases.customers_id = customers.id;
-
-SELECT customers.first_name, customers.last_name, items.price FROM items
-INNER JOIN purchases ON purchases.item_id = items.id
-INNER JOIN customers OM purchases.customers_id = customers.id;
-
-SELECT customers.first_name, customers.last_name, SUM(items.price) FROM items
-INNER JOIN purchases ON purchases.item_id = items.id
-INNER JOIN customers OM purchases.customers_id = customers.id
-GROUP BY customers.id;
-
-SELECT customers.first_name, customers.last_name, SUM(items.price) AS "total_spent" FROM items
-INNER JOIN purchases ON purchases.item_id = items.id
-INNER JOIN customers OM purchases.customers_id = customers.id
-GROUP BY customers.id
-ORDER BY total_spend;
-
-SELECT customers.first_name, customers.last_name, SUM(items.price) AS "total_spent" FROM items
-INNER JOIN purchases ON purchases.item_id = items.id
-INNER JOIN customers OM purchases.customers_id = customers.id
-GROUP BY customers.id
-ORDER BY total_spend DESC;
-
-SELECT customers.first_name, customers.last_name, SUM(items.price) AS "total_spent" FROM items
-INNER JOIN purchases ON purchases.item_id = items.id
-INNER JOIN customers OM purchases.customers_id = customers.id
-GROUP BY customers.id
-ORDER BY total_spend DESC
-LIMIT 1;
+# SELECT * FROM purchases INNER JOIN items on purchases.item_id = items.id;
+#
+# SELECT SUM(items.price) FROM purchases INNER JOIN items on purchases.item_id = items.id;
+#
+# SELECT * FROM items
+# INNER JOIN purchases ON purchases.item_id = items.id
+# INNER JOIN customers OM purchases.customers_id = customers.id;
+#
+# SELECT customers.first_name, customers.last_name, items.price FROM items
+# INNER JOIN purchases ON purchases.item_id = items.id
+# INNER JOIN customers OM purchases.customers_id = customers.id;
+#
+# SELECT customers.first_name, customers.last_name, SUM(items.price) FROM items
+# INNER JOIN purchases ON purchases.item_id = items.id
+# INNER JOIN customers OM purchases.customers_id = customers.id
+# GROUP BY customers.id;
+#
+# SELECT customers.first_name, customers.last_name, SUM(items.price) AS "total_spent" FROM items
+# INNER JOIN purchases ON purchases.item_id = items.id
+# INNER JOIN customers OM purchases.customers_id = customers.id
+# GROUP BY customers.id
+# ORDER BY total_spend;
+#
+# SELECT customers.first_name, customers.last_name, SUM(items.price) AS "total_spent" FROM items
+# INNER JOIN purchases ON purchases.item_id = items.id
+# INNER JOIN customers OM purchases.customers_id = customers.id
+# GROUP BY customers.id
+# ORDER BY total_spend DESC;
+#
+# SELECT customers.first_name, customers.last_name, SUM(items.price) AS "total_spent" FROM items
+# INNER JOIN purchases ON purchases.item_id = items.id
+# INNER JOIN customers OM purchases.customers_id = customers.id
+# GROUP BY customers.id
+# ORDER BY total_spend DESC
+# LIMIT 1;
