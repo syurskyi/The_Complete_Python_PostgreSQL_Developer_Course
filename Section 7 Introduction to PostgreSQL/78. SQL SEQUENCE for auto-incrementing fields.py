458633dac2e7c9f972a49dedad063fca4883bd33
syurskyi <sergejyurskyj@yahoo.com>
2019-10-30 17:34:35 -0700

    Create 78. SQL SEQUENCE for auto-incrementing fields.py

---
+++ Section 7 Introduction to PostgreSQL/78. SQL SEQUENCE for auto-incrementing fields.py
@@ -0,0 +1,17 @@
+# SELECT * FROM public.users;
+#
+# INSERT INTO public.users(name)
+# VALUES ('josesalvatiera')
+#
+# CREATE SEQUENCE users_id_seq START 3;
+#
+# ALTER TABLE public.users
+# ALTER COLUMN id
+# SET DEFAULT nextval('users_id_seq');
+#
+# ALTER SEQUENCE users_id_seq OWNED BY public.users.id;
+#
+# INSERT INTO public.users(name)
+# VALUES ('josesalvatiera')
+
+# SELECT * FROM public.users;
