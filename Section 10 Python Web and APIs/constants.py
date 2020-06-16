9c1b0071df1d9b4c8ab97904f3f296a19c175a7a
Grey <sergejyurskyj@yahoo.com>
2020-06-16 09:48:27 -0700

    Committed Section 10 Python Web and APIs

---
+++ Section 10 Python Web and APIs/constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = '6rMEPcsKZDNpNxKsY6hfNl6PY'
+CONSUMER_SECRET = '3B2V3xiYA2q0MQmPsuDQwCPdWqZccJiHlpC45dI07i8ulJMehh'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
